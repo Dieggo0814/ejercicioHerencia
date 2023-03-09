77708b6ef41dff5b1981a112fc26184449074e93
Dieggo <diego.fuentes@alu.uclm.es>
2023-03-09 11:12:11 +0100

    Creacion de metodos en Abogado

---
+++ src/Abogado.java
@@ -0,0 +1,20 @@
+public class Abogado extends Empleado{
+    @Override
+    public int getVacaciones() {
+        return super.getVacaciones()/2;
+    }
+
+    @Override
+    public double getSalario() {
+        return super.getSalario()+10000;
+    }
+    @Override
+    public int getHoras() {
+        return super.getHoras() + 5;
+    }
+
+    @Override
+    public String getMesVacaciones() {
+        return super.getMesVacaciones();
+    }
+}
